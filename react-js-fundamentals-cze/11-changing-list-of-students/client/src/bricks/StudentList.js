e08c47cd969d024df1719d70408424e635945126
Dudycha <jandudycha521@gmail.com>
2023-10-24 14:07:08 +0200

    implementation of filter

--- react-js-fundamentals-cze/11-changing-list-of-students/client/src/bricks/StudentList.js
+++ react-js-fundamentals-cze/11-changing-list-of-students/client/src/bricks/StudentList.js
@@ -1,36 +1,85 @@
-import React , { useState } from "react";
-import Student from "./Student";
+import React , { useState, useMemo } from "react";
+import StudentGridList from "./StudentGridList";
+import StudentTableList from "./StudentTableList";
+
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+import Icon from "@mdi/react";
+import { mdiTable, mdiViewGridOutline , mdiMagnify} from "@mdi/js";
+
+import Student from "./Student";
+
 
 
 function StudentList(props) {
   const [viewType, setViewType] = useState("grid");
   const isGrid = viewType === "grid";
+  const [searchBy, setSearchBy] = useState("");
+
+  const filteredStudentList = useMemo(() => {
+    return props.studentList.filter((item) => {
+      return (
+        item.firstname
+          .toLocaleLowerCase()
+          .includes(searchBy.toLocaleLowerCase()) ||
+        item.surname.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase())
+      );
+    });
+  }, [searchBy]);
+
+  function handleSearch(event) {
+    event.preventDefault();
+    setSearchBy(event.target["searchInput"].value);
+  }
+
+  function handleSearchDelete(event) {
+    if (!event.target.value) setSearchBy("");
+  }
 
   return (
     <div>
       <Navbar bg="light">
         <div className="container-fluid">
           <Navbar.Brand>Seznam studentů</Navbar.Brand>
-          <Button
-            variant="outline-primary"
-            onClick={() =>
-              setViewType((currentState) => {
-                if (currentState === "grid") return "table";
-                else return "grid";
-              })
-            }
-          >
-            <Icon size={1} path={isGrid ? mdiTable : mdiViewGridOutline} />{" "}
-            {isGrid ? "Tabulka" : "Grid"}
-          </Button>
+          <div>
+            <Form className="d-flex" onSubmit={handleSearch}>
+              <Form.Control
+                id={"searchInput"}
+                style={{ maxWidth: "150px" }}
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                onChange={handleSearchDelete}
+              />
+              <Button
+                style={{ marginRight: "8px" }}
+                variant="outline-success"
+                type="submit"
+              >
+                <Icon size={1} path={mdiMagnify} />
+              </Button>
+              <Button
+                variant="outline-primary"
+                onClick={() =>
+                  setViewType((currentState) => {
+                    if (currentState === "grid") return "table";
+                    else return "grid";
+                  })
+                }
+              >
+                <Icon size={1} path={isGrid ? mdiTable : mdiViewGridOutline} />{" "}
+                {isGrid ? "Tabulka" : "Grid"}
+              </Button>
+            </Form>
+          </div>
         </div>
       </Navbar>
       {isGrid ? (
-        <StudentGridList studentList={props.studentList} />
+        <StudentGridList studentList={filteredStudentList} />
       ) : (
-        <StudentTableList studentList={props.studentList} />
+        <StudentTableList studentList={filteredStudentList} />
       )}
     </div>
   );
