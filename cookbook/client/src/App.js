0e0da974ed6322e5eb1088fe22c657a47b40b192
Dudycha <jandudycha521@gmail.com>
2023-10-29 15:08:00 +0100

    loading recepies AND ingredients

--- cookbook/client/src/App.js
+++ cookbook/client/src/App.js
@@ -2,9 +2,6 @@ import "./App.css";
 import Banner from "./bricks/Banner";
 import CookbookList from "./bricks/CookbookList";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import burgerImg from './assets/burger.PNG';
-import cockaImg from './assets/čočka.PNG';
-import yummy from './assets/Sprite-0002.png';
 import { useEffect, useState } from "react";
 import Icon from "@mdi/react";
 import { mdiLoading } from "@mdi/js";
@@ -13,11 +10,19 @@ import styles from "./css/cookBook.module.css";
 const StateType = {
   SUCCESS: "success",
   ERROR: "error",
-  PENDING: "pending"  
+  PENDING: "pending"
+};
+
+const banner = {
+  name: "Kuchařka",
 };
 
 function App() {
-  const [cookBookLoadCall, setcookBookLoadCall] = useState({
+
+  const [recipeListCall, setRecipeListCall] = useState({
+    state: StateType.PENDING,
+  });
+  const [ingredientListCall, setIngredientListCall] = useState({
     state: StateType.PENDING,
   });
 
@@ -27,41 +32,59 @@ function App() {
     }).then(async (response) => {
       const responseJson = await response.json();
       if (response.status >= 400) {
-    
-        setcookBookLoadCall({ state: StateType.ERROR, error: responseJson });
+        setRecipeListCall({state: StateType.ERROR, error: responseJson});
       } else {
-        setcookBookLoadCall({ state: StateType.SUCCESS, data: responseJson });
+        setRecipeListCall({state: StateType.SUCCESS, data: responseJson});
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    fetch(`http://localhost:8000/ingredient/list`, {
+      method: "GET",
+    }).then(async (response) => {
+      const responseJson = await response.json();
+      if (response.status >= 400) {
+        setIngredientListCall({state: StateType.ERROR, error: responseJson});
+      } else {
+        setIngredientListCall({state: StateType.SUCCESS, data: responseJson});
       }
     });
   }, []);
 
   function getChild() {
-  
-    switch (cookBookLoadCall.state) {
-      case StateType.PENDING:
-        return (
-          <div className={styles.loading}>
-            <Icon size={2} path={mdiLoading} spin={true} />
-          </div>
-        );
-      case StateType.SUCCESS:
-        return (
-          <>
-           
-            <CookbookList cookbook={cookBookLoadCall.data} />
-          </>
-        );
-      case StateType.ERROR:
-        return (
-          <div className={styles.error}>
-            <div>Nepodařilo se načíst data o třídě.</div>
-            <br />
-            <pre>{JSON.stringify(cookBookLoadCall.error, null, 2)}</pre>
-          </div>
-        );
-      default:
-        return null;
+
+    if (ingredientListCall.state === StateType.PENDING || recipeListCall.state === StateType.PENDING) {
+      return (
+        <div className={styles.loading}>
+          <Icon size={2} path={mdiLoading} spin={true} />
+        </div>
+      );
+    } else if (ingredientListCall.state === StateType.ERROR || recipeListCall.state === StateType.ERROR) {
+      return (
+        <div className={styles.error}>
+           <div>Nepodařilo se načíst data o třídě.</div>
+          <div>Ingredients: {ingredientListCall.state}</div>
+          <div>Recepies: {recipeListCall.state}</div>
+         
+          <pre>{JSON.stringify(ingredientListCall.error, null, 2)}</pre>
+        </div>
+      );
+    } else if (ingredientListCall.state === StateType.SUCCESS || recipeListCall.state === StateType.SUCCESS) {
+      return (
+        <>
+         <Banner banner={banner}/>
+       
+         <CookbookList cookbook={ingredientListCall.data} />
+         
+         
+        </>
+      );
+    } else {
+      return null;
     }
+
+
   }
 
   return <div className="App">{getChild()}</div>;
