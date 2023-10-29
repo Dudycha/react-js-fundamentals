5d058a1d41bd49d777c268122c8f0a8ffe021dc1
Dudycha <jandudycha521@gmail.com>
2023-10-29 15:08:10 +0100

    Banner fixed

--- cookbook/client/src/bricks/Banner.js
+++ cookbook/client/src/bricks/Banner.js
@@ -4,14 +4,11 @@ import styles from "../css/cookBook.module.css";
 class Banner extends React.Component {
   render() {
     return (
-      <h1 className={styles.banner} >
-        Kuchařka{"  "}
-        <span className={styles.header}>
-          {this.props.classroom.name}
-        </span>
+      <h1 className={styles.banner}>
+        {this.props.banner.name}
       </h1>
     );
   }
-} 
+}
 
 export default Banner;
