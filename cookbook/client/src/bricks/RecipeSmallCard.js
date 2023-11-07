5d35fe5676ef2fe66609c4c03723e37a34b0c4bf
Dudycha <jandudycha521@gmail.com>
2023-11-07 21:43:25 +0100

    new features implemented for smallCard and TableLi

--- cookbook/client/src/bricks/RecipeSmallCard.js
+++ cookbook/client/src/bricks/RecipeSmallCard.js
@@ -1,9 +1,10 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Icon from "@mdi/react";
-import {mdiFoodForkDrink , mdiCog} from "@mdi/js";
+import { mdiFoodForkDrink, mdiCog } from "@mdi/js";
 import styles from "../css/recipe.module.css";
-import {shortenText} from "../helpers/common";
+import { shortenText } from "../helpers/common";
+import RecipeEditList from "../bricks/RecipeEditList";
 
 class RecipeSmallCard extends React.Component {
 
@@ -11,38 +12,42 @@ class RecipeSmallCard extends React.Component {
 
 
     return (
-        <div className={styles.recipeSmall}>
-          <Card className={styles.recipeSmallCard}>
-            <Card.Body className={styles.recipeSmallBody}>
-              <div>
-                <h2 className={styles.recipeSmallname}>
-                  <Icon path={mdiFoodForkDrink} size={2} color="red"/>{" "}
-                  {this.props.recipe.name}
-                </h2>
-                <img alt={this.props.recipe.name} src={this.props.recipe.imgUri} className={styles.recipeSmallImg}/>
+      <div className={styles.recipeSmall}>
 
-                <div className={styles.recipeDesc}>
-                  <div className={styles.recipeSmallDescIcon}>
-                    <Icon path={mdiCog} size={1.5} color="grey"/>{" "}
-                  </div>
+        <Card className={styles.recipeSmallCard}>
+          <Card.Body className={styles.recipeSmallBody}>
+            <div style={{ textAlign: 'right' }}>
+              <RecipeEditList recipe={this.props.recipe} />
+            </div>
+            <div>
+              <h2 className={styles.recipeSmallname}>
+                <Icon path={mdiFoodForkDrink} size={2} color="red" />{" "}
+                {this.props.recipe.name}
+              </h2>
+              <img alt={this.props.recipe.name} src={this.props.recipe.imgUri} className={styles.recipeSmallImg} />
 
-                  <div className={styles.recipeSmallDescTxt}>
-                    {shortenText(this.props.recipe.description, 50)}
-                    <ul className={styles.ingredientsList}>
-                      {this.props.recipe.ingredients.slice(0, 4).map((ingredient) =>{
-                            const foundIngredient = this.props.ingredientList.find((ingredientInList)=>ingredientInList.id === ingredient.id);
-                            return <li key={foundIngredient.id}>{foundIngredient.name}</li>;
-                          }
-                      )}
-                      {this.props.recipe.ingredients.length > 4 && <li>...</li>}
-                    </ul>
-                  </div>
-
+              <div className={styles.recipeDesc}>
+                <div className={styles.recipeSmallDescIcon}>
+                  <Icon path={mdiCog} size={1.5} color="grey" />{" "}
                 </div>
+
+                <div className={styles.recipeSmallDescTxt}>
+                  {shortenText(this.props.recipe.description, 50)}
+                  <ul className={styles.ingredientsList}>
+                    {this.props.recipe.ingredients.slice(0, 4).map((ingredient) => {
+                      const foundIngredient = this.props.ingredientList.find((ingredientInList) => ingredientInList.id === ingredient.id);
+                      return <li key={foundIngredient.id}>{foundIngredient.name}</li>;
+                    }
+                    )}
+                    {this.props.recipe.ingredients.length > 4 && <li>...</li>}
+                  </ul>
+                </div>
+
               </div>
-            </Card.Body>
-          </Card>
-        </div>
+            </div>
+          </Card.Body>
+        </Card>
+      </div>
     );
   }
 }
--- cookbook/client/src/bricks/RecipeTableList.js
+++ cookbook/client/src/bricks/RecipeTableList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 import {shortenText} from "../helpers/common";
+import RecipeEditList from "../bricks/RecipeEditList";
 
 function RecipeTableList(props) {
   return (
@@ -9,18 +10,23 @@ function RecipeTableList(props) {
         <tr>
           <th>Název receptu</th>
           <th>Popis receptu</th>
+         
         </tr>
         </thead>
         <tbody>
         {props.recipeList.map((recipe) => {
           return (
               <tr key={recipe.id}>
-                <td>{recipe.name}</td>
-                <td>{shortenText(recipe.description,150)}</td>
+               
+               <td style={{ fontWeight: 'bold' }}>{shortenText(recipe.name,45)}</td>
+                <td>{shortenText(recipe.description,190)}</td>
+                <td><RecipeEditList recipe={recipe} /></td>
               </tr>
           );
         })}
+       
         </tbody>
+       
       </Table>
   );
 }
